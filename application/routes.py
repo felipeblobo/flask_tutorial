b062ea0281b9add5b8475860a3117fa8a5716b58
Felipe Lobo <felipeblobo@gmail.com>
2021-03-22 22:08:49 -0300

    criando rota e tela de login

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
+from flask import render_template, flash, redirect
 from application import app
-from flask import render_template
+from application.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,13 @@ def index():
         }
     ]
     return render_template('index.html', user = user, posts = posts, title = "Home")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requisitado para o usuário {}, rememberMe={}'.format(
+            form.username.data, form.rememberMe.data
+        ))
+        return redirect ('/index')
+    return render_template('login.html', title = 'Login', form = form)
